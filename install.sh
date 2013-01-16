fe688e49ad6a5615c9d78132a7d0fd22ff4e4ceb
Tim Zehta <tim@clockwork.net>
2013-01-15 23:18:12 -0600

    updated documentation

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Install symlink of ssh-askpass.sh to /usr/libexec/ssh-askpass
+
 set -o errexit
 set -o errtrace
 set -o nounset
@@ -12,10 +15,11 @@ trap '_es=${?};
 dest=/usr/libexec/ssh-askpass
 src="$(pwd)/ssh-askpass.sh"
 
+# only install if there's nothing there, or it's a symlink
 if [[ ! -e "${dest}" ]] || [[ -L "${dest}" ]]
 then
     sudo ln -fns "${src}" "${dest}"
 else
-    echo "${dest} must be absent or a symlink for install"
+    echo "ERROR: ${dest} must be absent or a symlink for install"
     ls -l ${dest}
 fi
